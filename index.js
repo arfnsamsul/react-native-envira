a9744a1fb0227197bef8ed7118920f82c0fefa98
Samsul Arifin <arfn.samsul@gmail.com>
2019-08-01 15:41:38 +0700

    fix ios only library

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 
 import { Platform }     from 'react-native';
 
-const RNiBeaconsModule = Platform.select({
-  ios:      () => require('./lib/next/new.module.ios.js'),
-  android:  () => require('./lib/next/new.module.android.js')
-})();
+const RNiBeaconsModule = require('./lib/next/new.module.ios.js')
 
 export default RNiBeaconsModule;
